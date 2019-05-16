b3aafa161957a9b826a7f216e53bf0e7fdbbfac8
tiango <tnngou@gmail.com>
2019-05-16 11:52:05 +0800

    通过传入的count, 如果大于1自动转换为对应的毫秒和微秒, 最小单位1个/秒

--- bucket.go
+++ bucket.go
@@ -1,28 +1,61 @@
 package bucket
 
 import (
+	"math"
+	"time"
+
 	"github.com/tnngo/bucket/queue"
 )
 
 type Bucket struct {
+	// 最大极限值1000000, 即1微秒1个
 	count int
 
 	q *queue.Queue
+
+	duration time.Duration
+}
+
+func (b *Bucket) setDuration() *Bucket {
+	switch {
+	case b.count == 1:
+		b.duration = 1 * time.Second
+		return b
+	case b.count > 1 && b.count <= 1000:
+		t := (1.0 / float64(b.count)) * 1000.0
+		v := int(math.Floor(t + 0.5))
+		b.duration = v * time.Millisecond
+		return b
+	case b.count > 1000 && b.count <= 1000000:
+		t := (1.0 / float64(b.count)) * 1000.0 * 1000.0
+		v := int(math.Floor(t + 0.5))
+		b.duration = v * time.Microsecond
+		return b
+	default:
+		return b
+	}
 }
 
 // New 创建Bucket指针对象,
 func New(count int) *Bucket {
-	if count < 1 {
+	switch {
+	case count < 1:
 		panic("生产的令牌数不能小于1")
-	}
-	return &Bucket{
-		count: count,
-		q:     queue.New(),
+	case count > 1000000:
+		panic("生产的令牌数不能大于1000000")
+	default:
+		return (&Bucket{
+			count: count,
+			q:     queue.New(),
+		}).setDuration()
 	}
 }
 
 // Start
 func (b *Bucket) Start() {
+	for {
+		time.Sleep(b.duration)
+	}
 }
 
 // Acquire 获得令牌
