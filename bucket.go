fd8ff65e4129f2b34ecb112ad3854149142efc82
tiango <tnngou@gmail.com>
2019-05-16 12:24:43 +0800

    完成令牌存放和拿取的基本功能

--- bucket.go
+++ bucket.go
@@ -8,7 +8,6 @@ import (
 )
 
 type Bucket struct {
-	// 最大极限值1000000, 即1微秒1个
 	count int
 
 	q *queue.Queue
@@ -36,13 +35,16 @@ func (b *Bucket) setDuration() *Bucket {
 	}
 }
 
-// New 创建Bucket指针对象,
+// New 创建Bucket指针对象
+// count 最大极限值1000000, 即1微秒1个.
+// 事实上由于硬件或一些开销, 比如锁等,
+// 并不一定会在1微秒内生成1个, 可能会出现几十或几百毫秒生成1个
 func New(count int) *Bucket {
 	switch {
 	case count < 1:
 		panic("生产的令牌数不能小于1")
 	case count > 1000000:
-		panic("生产的令牌数不能大于1000000")
+		panic("1秒内生成的令牌数不能大于1000000")
 	default:
 		return (&Bucket{
 			count: count,
@@ -53,12 +55,16 @@ func New(count int) *Bucket {
 
 // Start
 func (b *Bucket) Start() {
+	n := 0
 	for {
+		n++
+		b.q.Put(n)
 		time.Sleep(b.duration)
 	}
 }
 
 // Acquire 获得令牌
 func (b *Bucket) Acquire() bool {
+	b.q.Take()
 	return false
 }
