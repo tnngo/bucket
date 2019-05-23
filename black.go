ba2901385aa75bb9ff831f4543c1cd5f24a6484d
tiango <tnngou@gmail.com>
2019-05-23 19:57:24 +0800

    定时计划在Black初始化时进行, 而不是放在Acquire中, 否则会导致为每个请求创建一个定时计划

--- black.go
+++ black.go
@@ -1,6 +1,7 @@
 package bucket
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -10,10 +11,22 @@ type Black struct {
 }
 
 func NewBlack(count int) *Black {
-	return &Black{
+	black := &Black{
 		b:      New(count),
 		ticker: time.NewTicker(1 * time.Second),
 	}
+
+	go func() {
+		for {
+			select {
+			case <-black.ticker.C:
+				//TODO 统计
+				fmt.Println("test")
+			}
+
+		}
+	}()
+	return black
 }
 
 // RedisOption Redis配置
@@ -28,21 +41,11 @@ func (black *Black) SetRedis(ro *RedisOption) {}
 /**
  ** 用于限制整个系统流量, 可以用于入口处,
  ** 无论是合法请求还是非法请求,
- ** 只要1个IP在1秒内拿走count/2个令牌,
+ ** 只要1个IP在1秒内拿走2/count个令牌,
  ** 则后续其他请求都将进行惩罚用来平衡系统开销,
  ** 直到屏蔽该IP后对其他IP进行速率恢复.
 **/
 // Acquire
 func (black *Black) Acquire(ip string) {
-	go func() {
-		for {
-			select {
-			case <-black.ticker.C:
-			default:
-				black.ticker.Stop()
-			}
-
-		}
-	}()
 	black.b.Acquire()
 }
